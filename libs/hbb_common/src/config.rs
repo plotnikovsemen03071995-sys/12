8c7cf6858c6d0ddf5330263ed2997e684d71d617
plotnikovsemen03071995-sys <plotnikov.semen.03071995@gmail.com>
2026-01-22 11:34:41 +0500

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["212.109.199.152"];
+pub const RS_PUB_KEY: &str = "85BflCEGXzEAAs1cScgR+uTNc7igIkqpUmq+vG5vffw=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
